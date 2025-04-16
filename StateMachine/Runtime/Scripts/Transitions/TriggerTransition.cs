53129a161dd4ecf100142a9fd7a47f681ae4b272
Skiif64 <Skiif64@yandex.ru>
2025-04-16 19:21:40 +0400

    Add trigger transition

---
+++ StateMachine/Runtime/Scripts/Transitions/TriggerTransition.cs
@@ -0,0 +1,30 @@
+using StateMachine.Abstractions;
+
+namespace StateMachine.Transitions
+{
+    public struct TriggerTransition<TContext> : ITransition<TContext>
+    {
+        private bool _triggered;
+        
+        public IState<TContext> TransitionTo { get; }
+
+        public TriggerTransition(IState<TContext> transitionTo)
+        {
+            TransitionTo = transitionTo;
+            _triggered = false;
+        }
+        
+        public bool IsSatisfied(IStateMachine<TContext> stateMachine)
+        {
+            if (!_triggered)
+            {
+                return false;
+            }
+
+            _triggered = false;
+            return true;
+        }
+
+        public void Trigger() => _triggered = true;
+    }
+}
